077d8b50b2dba9549ad4a3ea8f61720e4da34619
Nguyễn Phan Vũ <npvu1510@gmail.com>
2024-07-24 10:23:21 +0700

    thiết lập dự án reactjs typescript với eslint, prettier

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import styles from './app.module.scss'
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.header}>Welcome to my ReactJS Typescript project</h1>
+    </div>
+  )
+}
+
+export default App
